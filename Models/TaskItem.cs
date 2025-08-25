e4abf373f2794f671e392c24968cc68fef81005b
robertgruevski <robertgruevski@outlook.com>
2025-08-25 18:18:35 -0400

    Add validation, navigation for Tasks, and rename TaskItem to Task for each view.

--- Models/TaskItem.cs
+++ Models/TaskItem.cs
@@ -5,10 +5,13 @@ namespace TaskManagement.Models
 	public class TaskItem
 	{
 		public int Id { get; set; }
-		[Required]
+		[Required(ErrorMessage = "Title is required")]
+		[StringLength(50, ErrorMessage = "Title cannot be longer than 50 characters")]
 		public string Title { get; set; }
+		[StringLength(300, ErrorMessage = "Description cannot be longer than 200 characters")]
 		public string Description { get; set; }
 		[DataType(DataType.Date)]
+		[Required(ErrorMessage = "Due date is required")]
 		public DateTime DueDate { get; set; }
 		public bool IsCompleted { get; set; }
 	}
