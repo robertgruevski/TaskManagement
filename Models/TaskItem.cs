d4936db473bd79e09d31246e139649bef5f5edf4
robertgruevski <robertgruevski@outlook.com>
2025-08-25 17:51:40 -0400

    Add project files.

---
+++ Models/TaskItem.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TaskManagement.Models
+{
+	public class TaskItem
+	{
+		public int Id { get; set; }
+		[Required]
+		public string Title { get; set; }
+		public string Description { get; set; }
+		[DataType(DataType.Date)]
+		public DateTime DueDate { get; set; }
+		public bool IsCompleted { get; set; }
+	}
+}
