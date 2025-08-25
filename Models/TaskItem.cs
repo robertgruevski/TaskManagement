4c367a7ac3902e0ec4e241fcfef19ce3c40313b9
robertgruevski <robertgruevski@outlook.com>
2025-08-25 19:59:07 -0400

    Fixing display names

--- Models/TaskItem.cs
+++ Models/TaskItem.cs
@@ -1,18 +1,21 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 
 namespace TaskManagement.Models
 {
 	public class TaskItem
 	{
 		public int Id { get; set; }
-		[Required(ErrorMessage = "Title is required")]
-		[StringLength(50, ErrorMessage = "Title cannot be longer than 50 characters")]
+		[Required(ErrorMessage = "Title is required.")]
+		[StringLength(50, ErrorMessage = "Title cannot be longer than 50 characters.")]
 		public string Title { get; set; }
-		[StringLength(300, ErrorMessage = "Description cannot be longer than 200 characters")]
+		[StringLength(300, ErrorMessage = "Description cannot be longer than 200 characters.")]
 		public string Description { get; set; }
+		[DisplayName("Due Date")]
 		[DataType(DataType.Date)]
-		[Required(ErrorMessage = "Due date is required")]
+		[Required(ErrorMessage = "Due date is required.")]
 		public DateTime DueDate { get; set; }
+		[DisplayName("Complete")]
 		public bool IsCompleted { get; set; }
 	}
 }
